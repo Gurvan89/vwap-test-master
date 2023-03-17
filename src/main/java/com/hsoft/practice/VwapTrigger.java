3f351c0ae94e2cf28aa89ef7faf4dd6f16ef6953
Gurvan Giboire <gurvan@superlatief.nl>
2023-03-17 12:49:07 -0400

    Implement the solution

--- src/main/java/com/hsoft/practice/VwapTrigger.java
+++ src/main/java/com/hsoft/practice/VwapTrigger.java
@@ -1,9 +1,13 @@
 package com.hsoft.practice;
 
+import java.util.HashMap;
 
 import com.hsoft.api.MarketDataListener;
 import com.hsoft.api.PricingDataListener;
 import com.hsoft.api.VwapTriggerListener;
+import com.hsoft.model.Product;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Entry point for the candidate to resolve the exercise
@@ -12,8 +16,11 @@ public class VwapTrigger implements PricingDataListener, MarketDataListener {
 
   private final VwapTriggerListener vwapTriggerListener;
 
+  private static final Logger log = LogManager.getLogger(VwapTrigger.class);
+
   /**
-   * This constructor is mainly available to ease unit test by not having to provide a VwapTriggerListener
+   * This constructor is mainly available to ease unit test by not having to
+   * provide a VwapTriggerListener
    */
   protected VwapTrigger() {
     this.vwapTriggerListener = (productId, vwap, fairValue) -> {
@@ -25,17 +32,46 @@ public class VwapTrigger implements PricingDataListener, MarketDataListener {
     this.vwapTriggerListener = vwapTriggerListener;
   }
 
+  private HashMap<String, Product> products = new HashMap<String, Product>();
+
   @Override
   public void transactionOccurred(String productId, long quantity, double price) {
     // This method will be called when a new transaction is received
     // You can then perform your check
-    // And, if matching the requirement, notify the event via 'this.vwapTriggerListener.vwapTriggered(xxx);'
+    // And, if matching the requirement, notify the event via
+    // 'this.vwapTriggerListener.vwapTriggered(xxx);'
+    Product product = products.containsKey(productId)
+        ? products.get(productId)
+        : new Product(productId);
+    product.addTransaction(quantity, price);
+    this.applyVwapTriggered(product);
+
   }
 
   @Override
   public void fairValueChanged(String productId, double fairValue) {
     // This method will be called when a new fair value is received
     // You can then perform your check
-    // And, if matching the requirement, notify the event via 'this.vwapTriggerListener.vwapTriggered(xxx);'
+    // And, if matching the requirement, notify the event via
+    // 'this.vwapTriggerListener.vwapTriggered(xxx);'
+    Product product = products.containsKey(productId)
+        ? products.get(productId)
+        : new Product(productId);
+    product.setFairValue(fairValue);
+    this.applyVwapTriggered(product);
   }
+
+  /**
+   * Apply the wrap triggered if necessary.
+   * In any case save the last update of the product
+   * @param product Provided product
+   */
+  public void applyVwapTriggered(Product product) {
+    if (product.isVwapGreaterThanFairValue()) {
+      this.vwapTriggerListener.vwapTriggered(product.getProductId(), product.getCalculatedVwap(),
+          product.getFairValue());
+    }
+    products.put(product.getProductId(), product);
+  }
+
 }
